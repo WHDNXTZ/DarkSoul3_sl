7b921c623f0cb0ff978476d52688ac6eb1dc9301
WHDNXTZ <1791341935@qq,com>
2024-09-25 18:35:05 +0800

    Auto commit: 2024/09/25 周三 18:35:00.98

--- add_sl.bat
+++ add_sl.bat
@@ -1,25 +1,27 @@
 @echo off
-chcp 65001
 set repo_path=C:\Users\86188\AppData\Roaming\DarkSoulsIII
 cd /d "%repo_path%" || (
-    echo 无法找到指定路径。
+    echo Unable to find the specified path.
     exit /b
 )
-set /p use_default="欢迎来到黑魂云存档自动提交脚本，是否使用默认提交信息？(y/n): "
+echo Welcome to the Dark Souls cloud save auto commit script!
+set /p use_default="Do you want to use the default commit message? (y/n): "
 if "%use_default%"=="y" (
     set commit_message=Auto commit: %date% %time%
 ) else (
-    set /p commit_message="请输入提交信息: "
+    set /p commit_message="Please enter the commit message: "
 )
 
-set /p use_default_branch="是否从本地 master 分支提交到远程的 master 分支？(y/n): "
+set /p use_default_branch="Do you want to push from the local master branch to the remote master branch? (y/n): "
 if "%use_default_branch%"=="y" (
     git push origin master:master
 ) else (
-    set /p local_branch="请输入本地分支名称: "
-    set /p remote_branch="请输入远程分支名称: "
+    set /p local_branch="Please enter the local branch name: "
+    set /p remote_branch="Please enter the remote branch name: "
     git push origin %local_branch%:%remote_branch%
 )
 
 git add .
 git commit -m "%commit_message%"
+
+pause
